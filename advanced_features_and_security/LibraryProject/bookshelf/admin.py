551b8c4615d4e1fea4000ae58539cbd337f8b39d
Hajar Amj <hajaramj97@gmail.com>
2025-08-09 18:33:08 +0100

    add all

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from bookshelf.models import CustomUser
 from django.utils.translation import gettext_lazy as _
+from .admin_custom import CustomUserAdmin
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
