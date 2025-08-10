bd38c756c83de96d1d88c138ff5e914ec0bb3524
Hajar Amj <hajaramj97@gmail.com>
2025-08-10 22:22:02 +0100

    add all

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
+    """
+    GET /api/books/ -> list all Book instances
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
