a8624f1216fcfc47ea4a3879aeccdfbc2f8fdfa0
Hajar Amj <hajaramj97@gmail.com>
2025-08-10 22:31:03 +0100

    add all

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # existing list-only endpoint
     path('books/', BookList.as_view(), name='book-list'),
+
+    # router-managed endpoints for full CRUD
+    path('', include(router.urls)),
 ]
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,16 @@ class BookList(generics.ListAPIView):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    Full CRUD for Book:
+    - list:   GET  /books_all/
+    - create: POST /books_all/
+    - retrieve: GET /books_all/{pk}/
+    - update: PUT /books_all/{pk}/
+    - partial_update: PATCH /books_all/{pk}/
+    - destroy: DELETE /books_all/{pk}/
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
